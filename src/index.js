4d78d1c9f672cef21ec9d8b54d104f682a7d65ea
Kuper <13lars.cooper@gmail.com>
2019-02-27 17:22:49 +0300

    correct bin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
 const greeting = () => {
+  console.log('Welcome to the Brain Games!\n');
   const askName = () => readlineSync.question('May i have your name? ');
   console.log("Hello, " + askName() + "!");
 };
