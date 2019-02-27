b7b9b9a481a0b13498a352f358b31d29a0c71059
Kuper <13lars.cooper@gmail.com>
2019-02-27 19:21:47 +0300

    bin

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import readlineSync from 'readline-sync';
 const greeting = () => {
   console.log('Welcome to the Brain Games!\n');
   const askName = () => readlineSync.question('May i have your name? ');
-  console.log("Hello, " + askName() + "!");
+  console.log(`Hello, ${askName()}!`)
 };
 export default greeting;
